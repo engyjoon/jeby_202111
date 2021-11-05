e17d70a4f82ad76d27e40d89d868d59feec6344f
10048047 <yjkwon@kt.com>
2021-11-05 13:23:11 +0900

    로그아웃 기능 개발 완료

--- mysite/config/settings/base.py
+++ mysite/config/settings/base.py
@@ -55,6 +55,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'common',
+    'news',
 ]
 
 MIDDLEWARE = [
@@ -151,3 +153,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/news/list'
+LOGOUT_REDIRECT_URL = '/common/login'
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='common:login')
+def news_list(request):
+    """
+    뉴스 리스트를 반환한다.
+    """
+    return render(
+        request,
+        'news/news_list.html',
+    )
